d067476f1cd30a892c9530dd7d373eda0089a1fd
CarltonXu <hzxuxingzhuang@163.com>
2022-11-26 13:01:28 +0800

    Split the css file by module function

--- easyops/static/js/easyops.js
+++ easyops/static/js/easyops.js
@@ -11,7 +11,7 @@
       minute = minute < 10 ? "0" + minute : minute;
       var seconds = date.getSeconds();
       seconds = seconds < 10 ? "0" + seconds : seconds;
-      var format_time = "<span class='narbar-current-time-content'>" + hour + ":" + minute + ":" + seconds + "</span>";
+      var format_time = "<span class='navbar-current-time-content'>" + hour + ":" + minute + ":" + seconds + "</span>";
       var str = "现在时间: " + year + "年" + month + "月" + day + "日&nbsp" + format_time;
       $(".navbar-current-time").html(str);
     }, 1000);
